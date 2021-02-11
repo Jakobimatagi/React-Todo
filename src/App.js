2febc2ec1284bcb50858ff9f375c20da757c6fc5
Jakobi Matagi <jkmatagi@hotmail.com>
2021-02-10 17:12:54 -0700

    mvp complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,92 @@
 import React from 'react';
+import styled from 'styled-components'
+import TodoForm from "./components/TodoForm"
+import TodoList from "./components/TodoList"
 
+const todo = [
+]
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      todo: todo
+    }
+  }
+
+  toggleDone = (todoId) => {
+    const newTodo = this.state.todo.map(item => {
+      if (todoId === item.id) {
+        return{
+          ...item,
+          completed: !item.completed
+        }
+      }
+      else {return item;}
+    })
+    this.setState({
+      
+      ...this.state,
+      todo: newTodo
+    })
+  }
+
+  clearCompleted = () => {
+    console.log('cleared');
+
+    this.setState({
+      
+      ...this.state,
+      todo: this.state.todo.filter(item => !item.completed)
+    })
+  }
+
+  addTodo = (todoName, e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: Date.now(),
+      todo: todoName,
+      completed: false
+    }
+    this.setState({
+      ...this.state,
+      todo: [...this.state.todo, newTodo]
+    })
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
+      <StyledDiv>
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <div>
+        <h2>Todo App</h2>
+        
+        <TodoForm addTodo={this.addTodo} className='todoForm'/>
+        
+        </div>
+        <TodoList clearCompleted={this.clearCompleted} toggleDone={this.toggleDone} todo={this.state.todo} />
       </div>
+      </StyledDiv>
     );
   }
 }
 
 export default App;
+
+const StyledDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fe938c;
+  margin: 3em 25%;
+  
+h2{
+  font-size: 3rem;
+  display: flex;
+  border: 2px solid black;
+  padding: 1rem;
+}
+
+
+`
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,44 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+import styled from "styled-components";
+
+
+const TodoList = props => {
+    
+    
+    return(
+        <StyledDiv>
+        <div> {props.todo.map(item => (
+            <Todo toggleDone={props.toggleDone} key={item.id} todo={item} />
+        ))}
+        <button className="clearBtn" onClick={() => props.clearCompleted()}>Clear Completed</button>
+
+        </div>
+        </StyledDiv>
+    )
+}
+
+
+export default TodoList;
+
+const StyledDiv = styled.div`
+
+
+.clearBtn{
+    margin-left: 2.5em;
+    margin-bottom: 1em;
+    padding: .25em 1em;
+    font-size: 1em;
+    background-color: #F1DEDE;
+    border: none;
+}
+
+.clearBtn:hover{
+
+    background-color: #d496a7;
+}
+`
